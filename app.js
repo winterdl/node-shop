3bfc22b01dfcb104ad8245a6a29c1c03411c026e
Joe Alongi <collectedview@gmail.com>
2021-07-05 21:45:19 -0400

    Form Text

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
@@ -9,6 +11,12 @@ const server = http.createServer((req, res) => {
       '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
     res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
     return res.end();
   }
   res.setHeader("Content-Type", "text/html");
