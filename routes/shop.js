248b87ac3ffa592d909be1fc88009ae02b7aa5dd
Joe Alongi <collectedview@gmail.com>
2021-07-11 17:07:11 -0400

    Handlebars Layout

--- routes/shop.js
+++ routes/shop.js
@@ -1,19 +1,21 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
 
-const rootDir = require("../util/path");
-const adminData = require("./admin");
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
+router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render("shop", {
+  res.render('shop', {
     prods: products,
-    pageTitle: "Shop",
-    path: "/",
+    pageTitle: 'Shop',
+    path: '/',
     hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
   });
 });
 
