9a6fdbddd5d6d080e99b60beff5869b674c7db55
Joe Alongi <collectedview@gmail.com>
2021-07-11 16:14:58 -0400

    Pug Templates

--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,22 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
 
-const rootDir = require("../util/path");
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 const products = [];
 
 // /admin/add-product => GET
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product", { pageTitle: "Add Product" });
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
 // /admin/add-product => POST
-router.post("/add-product", (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   products.push({ title: req.body.title });
-  res.redirect("/");
+  res.redirect('/');
 });
 
 exports.routes = router;
