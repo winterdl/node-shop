03428d0d3454d9529d2f4b0c51c2f826eb173872
Joe Alongi <collectedview@gmail.com>
2021-07-10 21:22:09 -0400

    Admin Routing + HTML Views

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,18 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
-router.get("/add-product", (req, res, next) => {
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   );
 });
 
-router.post("/product", (req, res, next) => {
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
-  res.redirect("/");
+  res.redirect('/');
 });
 
 module.exports = router;
