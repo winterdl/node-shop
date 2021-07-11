650e8b8ba69cde882c016f3a4db6eaab21bd397b
Joe Alongi <collectedview@gmail.com>
2021-07-11 16:38:00 -0400

    Handlebars For -> Each

--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,20 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+  });
 });
 
 module.exports = router;
