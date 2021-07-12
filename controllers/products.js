8380e64acf6e2f603805f8765a652745d706b230
Joe Alongi <collectedview@gmail.com>
2021-07-11 21:48:10 -0400

    Controllers

--- controllers/products.js
+++ controllers/products.js
@@ -1,27 +1,27 @@
 const products = [];
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
     formsCSS: true,
     productCSS: true,
-    activeAddProduct: true,
+    activeAddProduct: true
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
   products.push({ title: req.body.title });
-  res.redirect("/");
+  res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render("shop", {
+  res.render('shop', {
     prods: products,
-    pageTitle: "Shop",
-    path: "/",
+    pageTitle: 'Shop',
+    path: '/',
     hasProducts: products.length > 0,
     activeShop: true,
-    productCSS: true,
+    productCSS: true
   });
 };
