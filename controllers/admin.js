f0e423f5bd5585952c4c3322a45927c7065d7f39
Joe Alongi <collectedview@gmail.com>
2021-07-18 12:10:09 -0400

    Database Connection

--- controllers/admin.js
+++ controllers/admin.js
@@ -30,29 +30,29 @@ exports.postAddProduct = (req, res, next) => {
     });
 };
 
-exports.getEditProduct = (req, res, next) => {
-  const editMode = req.query.edit;
-  if (!editMode) {
-    return res.redirect("/");
-  }
-  const prodId = req.params.productId;
-  req.user
-    .getProducts({ where: { id: prodId } })
-    // Product.findByPk(prodId)
-    .then((products) => {
-      const product = products[0];
-      if (!product) {
-        return res.redirect("/");
-      }
-      res.render("admin/edit-product", {
-        pageTitle: "Edit Product",
-        path: "/admin/edit-product",
-        editing: editMode,
-        product: product,
-      });
-    })
-    .catch((err) => console.log(err));
-};
+//exports.getEditProduct = (req, res, next) => {
+//  const editMode = req.query.edit;
+//  if (!editMode) {
+//    return res.redirect('/');
+//  }
+//  const prodId = req.params.productId;
+//  req.user
+//    .getProducts({ where: { id: prodId } })
+//    // Product.findById(prodId)
+//    .then(products => {
+//      const product = products[0];
+//      if (!product) {
+//        return res.redirect('/');
+//      }
+//      res.render('admin/edit-product', {
+//        pageTitle: 'Edit Product',
+//        path: '/admin/edit-product',
+//        editing: editMode,
+//        product: product
+//      });
+//    })
+//    .catch(err => console.log(err));
+//};
 
 exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
@@ -60,7 +60,7 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findByPk(prodId)
+  Product.findById(prodId)
     .then((product) => {
       product.title = updatedTitle;
       product.price = updatedPrice;
@@ -90,7 +90,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId)
+  Product.findById(prodId)
     .then((product) => {
       return product.destroy();
     })
--- util/database.js
+++ util/database.js
@@ -1,17 +1,29 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://collectedview:gxU1kwBHXjg!@cluster0.bibgc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    "mongodb+srv://collectedview:gxU1kwBHXjg!@cluster0.bibgc.mongodb.net/shop?retryWrites=true&w=majority"
   )
     .then((client) => {
       console.log("Connected!");
-      callback(client);
+      _db = client.db();
+      callback();
     })
     .catch((err) => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
