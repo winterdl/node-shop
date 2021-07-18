f5cee637e74bf745f8288b9ef7ab955604e9fd34
Joe Alongi <collectedview@gmail.com>
2021-07-18 14:02:11 -0400

    Create New User

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
 
 const app = express();
 
@@ -18,12 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  //User.findByPk(1)
-  //  .then((user) => {
-  //    req.user = user;
-  //    next();
-  //  })
-  //  .catch((err) => console.log(err));
+  User.findById("60f46c047461b1f9022eff15")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
   next();
 });
 
