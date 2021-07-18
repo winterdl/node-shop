d3d4a8226c9af8143ccacf22ad2dc014a16cfc1f
Joe Alongi <collectedview@gmail.com>
2021-07-18 19:29:48 -0400

    Product, Cart & User Data

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("60f46c047461b1f9022eff15")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
