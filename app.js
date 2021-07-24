f24f1ebccaa87da0a5b3b201608b3413ffbb564b
Joe Alongi <collectedview@gmail.com>
2021-07-24 15:53:36 -0400

    Add Express Session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -18,9 +19,12 @@ const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({ secret: "my secret", resave: false, saveUninitialized: false })
+);
 
 app.use((req, res, next) => {
-  User.findById("60f626c8dde3a2724871e77d")
+  User.findById("560f626c8dde3a2724871e77d")
     .then((user) => {
       req.user = user;
       next();
